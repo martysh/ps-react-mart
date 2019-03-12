c32fc89e65a770906a42d92bf58d1e9eaff731f5
Irina Abramova <inabramova@gmail.com>
2019-03-11 17:07:27 -0700

    hello world

---
+++ src/components/HelloWorld/HelloWorld.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function HelloWorld({message}) {
+  return (
+    <div>
+      Hello ${message}
+    </div>
+  )
+}
+
+HelloWorld.propTypes = {
+  /** Message to display */
+  message:PropTypes.string
+};
+
+export default HelloWorld;
