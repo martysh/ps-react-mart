bd858da01901c9f1f0269e865615724f078a65fb
Irina Abramova <inabramova@gmail.com>
2019-03-11 17:39:54 -0700

    path import trickery

--- src/components/HelloWorld/HelloWorld.js
+++ src/components/HelloWorld/HelloWorld.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+/** Syper lame hello world component */
 function HelloWorld({message}) {
   return (
     <div>
-      Hello ${message}
+      Hello {message}
     </div>
   )
 }
@@ -12,6 +13,10 @@ function HelloWorld({message}) {
 HelloWorld.propTypes = {
   /** Message to display */
   message:PropTypes.string
+};
+
+HelloWorld.defaultProps = {
+  message:'World'
 };
 
 export default HelloWorld;
---
+++ src/docs/examples/HelloWorld/ExampleHelloWorld.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import HelloWorld from "ps-react/HelloWorld ";
+
+/** custom message */
+function ExampleHelloWorld() {
+  return (
+    <HelloWorld message="javascript" />
+  )
+}
+
+export default ExampleHelloWorld
