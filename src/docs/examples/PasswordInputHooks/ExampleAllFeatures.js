d0f09652f29d36d38b8ba916888fc05bfff72ce8
Irina Abramova <inabramova@gmail.com>
2019-03-13 19:03:32 -0700

    hooks example

---
+++ src/docs/examples/PasswordInputHooks/ExampleAllFeatures.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PasswordInputHooks from 'ps-react/PasswordInputHooks';
+
+/** All features enabled */
+class ExampleAllFeatures extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      password: 'initial'
+    };
+  }
+
+  getQuality() {
+    const length = this.state.password.length;
+    return length > 10 ? 100 : length * 10;
+  }
+
+  render() {
+    return (
+      <div>
+        <PasswordInputHooks
+          htmlId="password-input-example-all-features"
+          name="password"
+          onChange={ event => this.setState({ password: event.target.value })}
+          value={this.state.password}
+          minLength={8}
+          placeholder="Enter password"
+          error="some error just happened"
+          showVisibilityToggle
+          quality={this.getQuality()}
+          {...this.props} />
+      </div>
+    )
+  }
+}
+
+export default ExampleAllFeatures;
