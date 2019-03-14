524946465f472a85510db5d09b513443eba7b72c
Irina Abramova <inabramova@gmail.com>
2019-03-13 19:03:21 -0700

    text input styled components

---
+++ src/docs/examples/TextInputStyledC/Example.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import TextInputStyledC from 'ps-react/TextInputStyledC';
+
+/** Required TextBox with error */
+export default class ExampleError extends React.Component {
+  render() {
+    return (
+      <TextInputStyledC
+        htmlId="example-optional"
+        label="First Name"
+        name="firstname"
+        onChange={() => {}}
+        required
+        error="First name is required."
+       />
+    )
+  }
+}
