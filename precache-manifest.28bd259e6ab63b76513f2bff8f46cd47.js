8c9f941153b1cdabffb92e1131c3731fa6814878
martysh <vatrushka@gmail.com>
2019-03-14 19:48:48 -0700

    Updates

---
+++ precache-manifest.28bd259e6ab63b76513f2bff8f46cd47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5a8c2ae102f711907530",
+    "url": "/static/js/main.bffc3e0d.chunk.js"
+  },
+  {
+    "revision": "1ab9de4b78f629c39b56",
+    "url": "/static/js/2.2235bcfc.chunk.js"
+  },
+  {
+    "revision": "5a8c2ae102f711907530",
+    "url": "/static/css/main.5b4e9b40.chunk.css"
+  },
+  {
+    "revision": "1ab9de4b78f629c39b56",
+    "url": "/static/css/2.fb50aaf0.chunk.css"
+  },
+  {
+    "revision": "d7c928ead0251732008a646ebd54a295",
+    "url": "/index.html"
+  }
+];
