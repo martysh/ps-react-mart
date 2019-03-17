b3424f508070acb3d6cfe1e27a31b3bf6cbcbadc
martysh <vatrushka@gmail.com>
2019-03-17 16:00:42 -0700

    better close button?? I dunno..

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -20,19 +20,38 @@ const StyledModal = styled.div`
   `}
   .overlay-inner {
     background: white;
-    width: 700px;
+    overflow: auto;
+    margin: 48px;
+    width: 500px;
     min-height: 30vh;
     max-width: 80vw;
-    padding: 20px;
+    padding: 2em 3em;
     position: relative;
+    box-shadow: 0px 11px 15px -7px rgba(0,0,0,0.2), 0px 24px 38px 3px rgba(0,0,0,0.14), 0px 9px 46px 8px rgba(0,0,0,0.12);
+}
   }
   .close {
     position: absolute;
-    top: 0.2em;
-    right: 0.2em;
+    top: 0.3em;
+    right: 0.3em;
+    padding: 0.3em;
+    font-size: 2rem;
+    height: 1em;
+    width: 1em;
+    text-indent: 10em;
+    overflow: hidden;
     background: none;
     color: black;
     border: 0;
+    cursor: pointer;
+  }
+  .close::after{
+    position: absolute;
+    line-height: 0.5;
+    top: 0.2em;
+    left: 0.2em;
+    text-indent: 0;
+    content:'\00D7';
   }
 `;
 
@@ -55,9 +74,7 @@ function Modal(props) {
   return (
     <StyledModal open={open}>
       <div ref={node} className="overlay-inner">
-        <button type="button" className="close" onClick={toggleModal}>
-          × Close
-        </button>
+        <button type="button" className="close" onClick={toggleModal}>Close</button>
         {children}
       </div>
     </StyledModal>
