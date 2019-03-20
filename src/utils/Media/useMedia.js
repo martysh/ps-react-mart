f2e8b9fb9280878c881f820ab3fc982bdd32f1f1
martysh <vatrushka@gmail.com>
2019-03-19 21:13:26 -0700

    linter

--- src/utils/Media/useMedia.js
+++ src/utils/Media/useMedia.js
@@ -1,18 +1,18 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
 
 function useMedia(query) {
-  let [matches, setMatches] = useState(window.matchMedia(query).matches);
+  const [matches, setMatches] = useState(window.matchMedia(query).matches);
   useEffect(() => {
-    let media = window.matchMedia(query);
-    if (media.matches !== matches){
+    const media = window.matchMedia(query);
+    if (media.matches !== matches) {
       setMatches(media.matches);
     }
-    let listener = ()=> setMatches(media.matches);
+    const listener = () => setMatches(media.matches);
     media.addListener(listener);
     return () => {
       media.removeListener(listener);
-    }
-  }, [query])
+    };
+  }, [query]);
   return matches;
 }
 
